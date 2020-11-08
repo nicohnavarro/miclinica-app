999b1386e1badbb85561b7aa4556f2b8ec7508a2
nicohnavarro <nicohnavarro@gmail.com>
2020-11-08 02:09:52 -0300

    add captcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RecaptchaModule } from 'ng-recaptcha';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -40,6 +40,7 @@ import { StepperFormComponent } from './components/shared/stepper-form/stepper-f
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
     AngularMaterialModule,
+    RecaptchaModule,
     MatCarouselModule.forRoot()
   ],
   providers: [],
